6eb484f8dabd35285900a39b049938b7b87d9e68
Samul333 <samulshrestha101@gmail.com>
2021-11-21 22:04:01 +0545

    added a server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const app = express();
+
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+  });
