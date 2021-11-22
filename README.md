077f5c4ad0cbdde5e3e10230e5cd4806cca0104e
Samul333 <samulshrestha101@gmail.com>
2021-11-22 20:29:31 +0545

    Addede lines to the readme

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 This is a recreation of the super mario game in vanilla javascript 
 
+
+Implemented Features
+
+-Dynamically load level from a json file.
+-Dynamically load sprites.
+-Collision detection.
+-Simple mario physics
+
 Available on :
 
 https://super-mario-js.herokuapp.com/
--- public/js/main.js
+++ public/js/main.js
@@ -1,81 +1,42 @@
 import { createMario } from "./entities.js";
 import { setUpKeyboard } from "./input.js";
-import KeyboardState from "./ketboard.js";
+
 import { createCollisionLayer } from "./layers.js";
 
-
-import { loadLevel} from "./loader.js";
+import { loadLevel } from "./loader.js";
 
 import Timer from "./timer.js";
 
-
-
-
-
-
-
-const canvas = document.getElementById('screen');
+const canvas = document.getElementById("screen");
 /** @type {CanvasRenderingContext2D} */
-const context = canvas.getContext('2d')
+const context = canvas.getContext("2d");
 
+Promise.all([createMario(), loadLevel("1-1")]).then(([mario, level]) => {
+  level.entities.add(mario);
+  level.comp.layers.push(createCollisionLayer(level));
 
+  const input = setUpKeyboard(mario);
 
+  input.listenTo(window);
 
+  ["mousedown", "mousemove"].forEach((eventName) => {
+    canvas.addEventListener(eventName, (event) => {
+      if (event.buttons === 1) {
+        mario.vel.set(0, 0);
+        mario.pos.set(event.offsetX, event.offsetY);
+      }
+    });
+  });
 
-
-
-Promise.all([
-    createMario(),
-    loadLevel('1-1')
-])
-.then(([mario,level])=>{
-
-     level.entities.add(mario);
-    level.comp.layers.push(createCollisionLayer(level));
-
-    const input = setUpKeyboard(mario);
-
-    input.listenTo(window);
-
-    ['mousedown','mousemove'].forEach(eventName=>{
-        canvas.addEventListener(eventName,event=>{
-
-           
-            if(event.buttons===1){
-
-                mario.vel.set(0,0)
-                mario.pos.set(event.offsetX,event.offsetY);
-
-            }
-        })
-    })
-
-
-    mario.pos.set(64,180)
+  mario.pos.set(64, 180);
   // mario.vel.set(200,-600)
 
-      const timer = new Timer(1/60)
+  const timer = new Timer(1 / 60);
 
-    timer.update  = function update(deltaTime){
-         
-            level.update(deltaTime);
-            level.comp.draw(context)
-          
-    }
+  timer.update = function update(deltaTime) {
+    level.update(deltaTime);
+    level.comp.draw(context);
+  };
 
-   timer.start();
-})
-
-
-
-
-        
-     
-    
-
-
-
-
-
-
-    
+  timer.start();
+});
